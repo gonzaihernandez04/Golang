1c40ec9f0f392f421a06fe1ded77570f8b7840f3
Gonzalo <hernandezgonzalo5845@gmail.com>
2024-03-21 16:32:50 -0300

    union inter terminado

--- GUIA0/arreglos/escalar/escalar.go
+++ GUIA0/arreglos/escalar/escalar.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	var vector1 []int = []int{3, 2, 1}
+	var vector2 []int = []int{6, 5, 4}
+	Escalar(vector1, vector2)
+}
+
+func Escalar(vector1 []int, vector2 []int) (int, error) {
+	if len(vector1) != len(vector2) {
+		fmt.Println("Los vectores deben ser del mismo tamaño")
+		return 0, errors.New("los vectores deben ser del mismo tamaño")
+	}
+	var productoEscalar int = 0
+
+	for i := 0; i < len(vector1); i++ {
+		productoEscalar += (vector1[i] * vector2[i])
+	}
+
+	fmt.Printf("La suma de producto escalar de los vectores es de: %v: ", productoEscalar)
+	return productoEscalar, nil
+}
--- GUIA0/arreglos/escalar/escalar_test.go
+++ GUIA0/arreglos/escalar/escalar_test.go
@@ -7,6 +7,17 @@ import (
 )
 
 func TestSumaArreglo(t *testing.T) {
+	var vector1 []int = []int{3, 2, 1}
+	var vector2 []int = []int{6, 5, 4}
+	result, err := Escalar(vector1, vector2)
+	assert.Equal(t, result, 32)
+	assert.Equal(t, err, nil)
+}
 
-	assert.Equal(t, "hola", "hola")
+func TestNoSonIguales(t *testing.T) {
+	var vector1 []int = []int{3, 2, 1}
+	var vector2 []int = []int{6, 5, 9, 4}
+	result, err := Escalar(vector1, vector2)
+	assert.Equal(t, result, 0)
+	assert.Error(t, err, "los vectores deben ser del mismo tamaño")
 }
